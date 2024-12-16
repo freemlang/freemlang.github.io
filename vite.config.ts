1276f63863d8d493c70bba852c85ccdad0643842
PhillY <philly706869@gmail.com>
2024-12-17 01:20:50 +0900

    set configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  appType: "spa",
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess(),
+    }),
+  ],
+  root: path.resolve("src"),
+  publicDir: path.resolve("public"),
 });
